c0219b35f260c0f16a4ffafae4ea41137e9a2aa7
Kristian Mide <kristian@mide.dk>
2026-04-14 21:32:15 +0200

    Added git ignore and conf

---
+++ esp32ui/include/app_config.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Copy to app_config.h and fill in your local values before flashing.
+#define APP_WIFI_SSID "your-wifi-ssid"
+#define APP_WIFI_PASSWORD "your-wifi-password"
+#define APP_DAEMON_URL "http://192.168.1.100:8090"
+#define APP_API_TOKEN "replace-with-daemon-token"
+#define APP_TIMEZONE "CET-1CEST,M3.5.0/2,M10.5.0/3"
